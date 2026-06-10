36e69c9b5525320c0393b74d9a3ce66cbd6a157f
Vladyslav Yurchak <yurchakvladyslav@gmail.com>
2026-06-10 14:11:36 +0300

    Rename teacher students controller namespace

--- app/Http/Controllers/Admin/TeacherStudents/MyStudentsController.php
+++ app/Http/Controllers/Admin/TeacherStudents/MyStudentsController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Admin\Teacher_students;
+namespace App\Http\Controllers\Admin\TeacherStudents;
 
 use App\Http\Controllers\Controller;
 use App\Models\Student;
--- routes/teacher/students.php
+++ routes/teacher/students.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['teacher'])->group(function () {
-    Route::get('admin/my-students', \App\Http\Controllers\Admin\Teacher_students\MyStudentsController::class)->name('admin.teacher.my_students');
+    Route::get('admin/my-students', \App\Http\Controllers\Admin\TeacherStudents\MyStudentsController::class)->name('admin.teacher.my_students');
 });
