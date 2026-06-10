e181b9c34bf00176e47bee647a18d3263146cb38
Vladyslav Yurchak <yurchakvladyslav@gmail.com>
2026-06-10 14:29:14 +0300

    Normalize admin data route URL

--- routes/admin/data.php
+++ routes/admin/data.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['admin'])->group(function () {
-    Route::get('data', \App\Http\Controllers\Admin\Data\IndexController::class)
+    Route::get('admin/data', \App\Http\Controllers\Admin\Data\IndexController::class)
         ->name('admin.data.index');
     Route::get('admin/data/student-attendance/{student}', \App\Http\Controllers\Admin\Data\AttendanceController::class);
 });
